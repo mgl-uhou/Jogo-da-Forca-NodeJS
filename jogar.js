f6cb305e4ad8458fc8948e029ccd06f4379e2272
mgl-uhou <miguelferreiralaurentino@gmail.com>
2024-01-12 15:49:47 -0300

    Separando a função Jogar do script principal

---
+++ jogar.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const Jogar = (jogo) => {
+    let indice = Math.floor(Math.random() * jogo.palavras.length);
+    let escolhida = jogo.palavras[indice];
+
+    console.log(escolhida.getCaracteres())
+
+    do {
+        console.log(`${escolhida.getTentativas() + 1}ª tentativa:`);
+        suaTentativa = prompt('Digite uma letra: ').toUpperCase();
+        escolhida.setTeste(suaTentativa, jogo);
+    } while (jogo.getAndamento() != false);
+}
+
+module.exports = Jogar;
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const prompt = require('prompt-sync')();
 const { Palavra, Jogo } = require('./classes.js');
+const jogar = require("./jogar.js");
 
 let palavra1 = new Palavra("PURPURA");
 let palavra2 = new Palavra("CONDICAO");
@@ -10,7 +10,7 @@ jogo.palavras = [palavra1, palavra2, palavra3]
 let suaTentativa = '';
 const quantErros = 10;
 
-const jogar = () => {
+/* const jogar = () => {
     let indice = Math.floor(Math.random() * jogo.palavras.length);
     let escolhida = jogo.palavras[indice];
 
@@ -21,8 +21,8 @@ const jogar = () => {
         suaTentativa = prompt('Digite uma letra: ').toUpperCase();
         escolhida.setTeste(suaTentativa, jogo);
     } while (jogo.getAndamento() != false);
-}
+} */
 
 console.log('Seja bem-vindo ao Jogo da Forca em JavaScript, aqui, você deve adivinhar a palavra digitando letra por letra, ou até mesmo a palavra completa, em até 10 tentativas, desconsiderando acentos e pontuação.')
 
-jogar()
+jogar(jogo)
